88bbe83c9fa71aaa645f1e0dad2bf265c8a08970
ElenaAnnibali <77542450+ElenaAnnibali@users.noreply.github.com>
2022-05-23 11:28:33 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Guest List</h1>
+      <form>
+        <label>
+          <input />
+        </label>
+        <label>
+          <input />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default App;
